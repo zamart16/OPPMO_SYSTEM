cf0d28a167a286c1f2288b95cd455ec781f993cd
zamart16 <154574772+zamart16@users.noreply.github.com>
2026-02-20 16:06:15 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="icon" href="{{asset('logo.png')}}">
   <title>Supplier Evaluation Management</title>
   <script src="https://cdn.tailwindcss.com/3.4.16"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
